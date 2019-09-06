dd625d97af6a06d1d7eae963a104028ea4f879f0
jcunanan05 <j_cunanan05@yahoo.com>
2019-09-06 13:10:12 -0400

    Styled layouts for time length adjuster component, WIP

--- src/Timer/Adjuster/index.js
+++ src/Timer/Adjuster/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import './Adjuster.css';
 
 function Adjuster(props) {
   return (
     <section className="adjuster">
-      <h3>{props.title}</h3>
-      <button>+</button>
-      <p>5</p>
-      <button>-</button>
+      <h3 className="title">{props.title}</h3>
+      <div className="functions">
+        <button className="subtract">-</button>
+        <p className="text-color-grey--dark">
+          <span className="minute-value">5</span> min
+        </p>
+        <button className="add">+</button>
+      </div>
     </section>
   );
 }
