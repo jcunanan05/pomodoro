4bfb0911f2abb3ba54f5704b3df16e2f7a3ab977
jcunanan05 <cunanan.jonathan1995@gmail.com>
2019-10-19 00:36:32 -0400

    Add footer and styling

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,18 @@ import React from 'react';
 import './Layout.css';
 
 function Layout(props) {
-  return <main className="main fullscreen">{props.children}</main>;
+  const githubUrl = 'https://github.com/jcunanan05';
+  return (
+    <>
+      <main className="main fullscreen">{props.children}</main>
+      <footer className="footer">
+        <p>
+          Made by <a href={githubUrl}>Jonathan Cunanan.</a> Copyright&copy;
+          2019. All rights reserved.
+        </p>
+      </footer>
+    </>
+  );
 }
 
 export default Layout;
